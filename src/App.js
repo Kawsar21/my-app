ec2733c6fef1c3ee01503b8c4f672fc1c6b09c16
kawsar21 <knkawsar21@gmail.com>
2021-05-26 21:26:30 +0600

    first react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       <h2>Le's build a react app🚀</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
